5357eb8d6a054559d9e4ffa1b0fc74c4919ec290
eljan <dev.eljanrustamov@gmail.com>
2024-12-17 00:36:07 +0400

    refactor: clean up unnecessary JSX in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,5 @@
 const App = () => {
-  return (
-    <div className='min-h-screen flex flex-col justify-center items-center'>
-      <h1 className='text-3xl text-primary font-heading font-bold'>
-        Nazli Barbershop is one the best barbershop in Warsaw, Poland!
-      </h1>
-      <p className=' w-10/12 text-primary-light mt-5 font-body'>
-        Nazli Barbershop is one the best barbershop in Warsaw, Poland!Nazli
-        Barbershop is one the best barbershop in Warsaw, Poland!Nazli Barbershop
-        is one the best barbershop in Warsaw, Poland!Nazli Barbershop is one the
-        best barbershop in Warsaw, Poland!Nazli Barbershop is one the best
-        barbershop in Warsaw, Poland!
-      </p>
-      <p className=' w-10/12 text-primary-light mt-5'>
-        Nazli Barbershop is one the best barbershop in Warsaw, Poland!Nazli
-        Barbershop is one the best barbershop in Warsaw, Poland!Nazli Barbershop
-        is one the best barbershop in Warsaw, Poland!Nazli Barbershop is one the
-        best barbershop in Warsaw, Poland!Nazli Barbershop is one the best
-        barbershop in Warsaw, Poland!
-      </p>
-
-      <button className='text-red-500 border border-primary-light text-text p-5'>
-        BOOK NOW
-      </button>
-    </div>
-  );
+  return <div className=''></div>;
 };
 
 export default App;
