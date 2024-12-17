37db10d37cae1d489855e7e7bfa900dc140aa943
eljan <dev.eljanrustamov@gmail.com>
2024-12-17 23:36:22 +0400

    feat: implement routing and shared layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Suspense } from 'react';
+import { BrowserRouter, useRoutes } from 'react-router-dom';
+import { routes } from './routes/routes';
+
+const AppRoutes = () => useRoutes(routes);
+
 const App = () => {
-  return <div className=''></div>;
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<div className='p-6 text-center'>Loading...</div>}>
+        <AppRoutes />
+      </Suspense>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/routes/lazyPages.ts
@@ -0,0 +1,6 @@
+import { lazy } from 'react';
+
+export const HomePage = lazy(() => import('../pages/HomePage'));
+export const ServicesPage = lazy(() => import('../pages/ServicesPage'));
+export const ContactPage = lazy(() => import('../pages/ContactPage'));
+export const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
